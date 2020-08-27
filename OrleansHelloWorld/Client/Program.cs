336bbb2a5b1d8831fb23dfd9f33bc82774ecf365
BobCatCut <67309750+BobCatCut@users.noreply.github.com>
2020-08-27 03:05:48 +0300

    Add files via upload

---
+++ OrleansHelloWorld/Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using System.Threading.Tasks;
+using GrainInterfaces;
+
+namespace Client
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await ConnectClient())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nВиняток пiд час спроби запустити Client: {e.Message}");
+                Console.WriteLine("Переконайтеся, що працює Silo, до якого намагається пiдключитися Client.");
+                Console.WriteLine("\nНатиснiть будь-яку клавiшу для виходу.");
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> ConnectClient()
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansBasics";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Успiшне пiдключення до silo host \n");
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            // example of calling grains from the initialized client
+            var friend = client.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Привiт!");
+            Console.WriteLine("\n\n{0}\n\n", response);
+        }
+    }
+}
---
+++ OrleansHelloWorld/Grains/HelloGrain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using GrainInterfaces;
+
+namespace Grains
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+        Task<string> IHello.SayHello(string greeting)
+        {
+            logger.LogInformation($"\n Привiтливе повiдомлення: '{greeting}'");
+            return Task.FromResult($"\n Кожен день кажу: '{greeting}', отже все в порядку.");
+        }
+    }
+}
